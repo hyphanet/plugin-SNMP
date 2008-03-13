e9701a5ff66afc2e84a5ad5b37ec3603336b4024
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-13 20:40:07 +0000

    Make IOStatisticCollector non-static
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/SNMP@18515 67a373e5-eb02-0410-a15c-ee090a768436

--- snmplib/DataStatisticsInfo.java
+++ snmplib/DataStatisticsInfo.java
@@ -6,12 +6,14 @@ public class DataStatisticsInfo implements DataFetcher {
 	private String OID;
 	int blocks;
 	boolean in;
+	private final IOStatisticCollector collector;
 	
-	public DataStatisticsInfo(int blocks, boolean in) {
+	public DataStatisticsInfo(int blocks, boolean in, IOStatisticCollector collector) {
 		this.OID = "1.1." + blocks + '.' + (in?"1":"0");
 		//System.err.println("adding: " + this.OID);
 		this.in = in;
 		this.blocks = blocks;
+		this.collector = collector;
 	}
 	
 	public String getSNMPOID() {
@@ -21,12 +23,12 @@ public class DataStatisticsInfo implements DataFetcher {
 
 	public Object getSNMPData() {
 		if (blocks == 0) {
-			long io[] = IOStatisticCollector.getTotalIO();
+			long io[] = collector.getTotalIO();
 			return new SNMPCounter32(io[in?1:0]);
 		}
 		// else sum all fields up to <blocks>
 		int res = 0;
-		int stats[][] = IOStatisticCollector.getTotalStatistics();
+		int stats[][] = collector.getTotalStatistics();
 		for (int i = 0 ; i < blocks ; i++)
 			res += stats[i][in?1:0];
 		
